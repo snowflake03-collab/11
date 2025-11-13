c2494ae4e26d1909ce6f17ec0f66bd2eb1402733
snowflake03-collab <snowflake03@sookmyung.ac.kr>
2025-11-13 18:34:47 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  int i = 10;
+  char c = 'a';
+  
+  int * iptr;
+  iptr = &i;
+  char * cptr;
+  cptr = &c;
+  int * iptr2;
+  iptr2 = iptr;
+  
+  printf("i: %p\n%p (size: %i)\n", iptr, &i, sizeof(iptr));
+  printf("c: %p\n%p (size: %i)\n", cptr, &c, sizeof(cptr));
+  printf("iptr2: %p, %i\n", iptr, *iptr);
+  
+  system("PAUSE");	
+  return 0;
+}
