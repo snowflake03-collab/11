dcd925be72cb6ec4dc84400ecac2fa9d93aa54eb
snowflake03-collab <snowflake03@sookmyung.ac.kr>
2025-11-16 23:14:01 +0900

    11-3

--- main.c
+++ main.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void swap(int * x, int * y){
+     int temp;
+     temp = *x;
+     *x = *y;
+     *y = temp;
+     
+}
+
 int main(void)
 {
-  int i = 300;
+  int a = 3;
+  int b = 5;
   
-  int * pi = &i;
-  char * pc = &i;
-  
-  printf("%i, %i, %i\n", i, *pi, *pc);
+  swap(&a , &b);
+  printf("a:%i, b:%i\n", a, b);
   
   system("PAUSE");	
   return 0;
